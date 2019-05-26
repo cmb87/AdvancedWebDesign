31d451aada6c08e9cb0dce7b255bf7872ae5a536
cmb87 <christian.peeren@rwth-aachen.de>
2019-05-26 17:56:46 +0200

    increase functionality added to DB

--- src/models/database/database.py
+++ src/models/database/database.py
@@ -1,10 +1,34 @@
 import sqlite3 as lite
 import sys
+import uuid
 
 
 class Database(object):
     def __init__(self):
         self.database = 'test.db'
+
+    def _checkIfTableExists(self, tablename):
+        """
+        """
+        con = lite.connect(self.database)
+        with con:
+            cur = con.cursor()
+            cur.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{}'".format(tablename))
+            if cur.fetchone()[0] == 1:
+                return True
+        return False
+
+    def delete_table(self, tablename):
+        """
+        """
+        con = lite.connect(self.database)
+        with con:
+            # From the connection, we get the cursor object. The cursor is used
+            # to traverse the records from the result set. We call the execute()
+            # method of the cursor and execute the SQL statement.
+            cur = con.cursor()
+            cur.execute("DROP TABLE IF EXISTS {}".format(tablename))
+            print("-> Table removed!")
 
     def create_table(self, tablename, columnsdict, purge=False):
         """
@@ -14,17 +38,17 @@ class Database(object):
         """
         columns = ["{} {}".format(key, columnsdict[key]) for key in columnsdict.keys()]
 
-        con = lite.connect(self.database)
-        with con:
-            # From the connection, we get the cursor object. The cursor is used
-            # to traverse the records from the result set. We call the execute()
-            # method of the cursor and execute the SQL statement.
-            cur = con.cursor()
-            if purge:
-                cur.execute("DROP TABLE IF EXISTS {}".format(tablename))
-            cur.execute("CREATE TABLE {}({})".format(tablename, ', '.join(columns)))
-
-            print("-> Table created!")
+        if not self._checkIfTableExists(tablename):
+            con = lite.connect(self.database)
+            with con:
+                # From the connection, we get the cursor object. The cursor is used
+                # to traverse the records from the result set. We call the execute()
+                # method of the cursor and execute the SQL statement.
+                cur = con.cursor()
+                cur.execute("CREATE TABLE {}({})".format(tablename, ', '.join(columns)))
+                print("-> Table created!")
+        else:
+            print("-> Table exists omitting!")
 
     def insert(self, tablename, rows):
         """
@@ -51,20 +75,11 @@ class Database(object):
             cur.execute("SELECT * FROM {}".format(tablename))
             return cur.fetchone().keys()
 
-    def fetchAll(self, tablename, columns=None, query=None, mode="fetchall"):
+    def fetch(self, tablename, columns=None, query=None, mode="fetchall"):
         """
         """
-        columnNames = self.getColumnNames(tablename)
-
-        if isinstance(columns, type(None)):
-            columns = '*'
-        elif isinstance(columns, list):
-            columns = ', '.join(columns)
-
-        if isinstance(query, type(None)):
-            query = ''
-        if isinstance(query, str):
-            query = 'WHERE ' + query
+        columns = Database.columnsParser(columns)
+        query = Database.queryParser(query)
 
         con = lite.connect(self.database)
         with con:
@@ -82,6 +97,16 @@ class Database(object):
             else:
                 print("Mode unknown")
 
+    def remove(self, tablename, query=None):
+        """
+        """
+        query = Database.queryParser(query)
+
+        con = lite.connect(self.database)
+        with con:
+            cur = con.cursor()
+            cur.execute("DELETE FROM {} {}".format(tablename, query))
+
     def getMetaData(self, tablename):
         """
         """
@@ -93,16 +118,34 @@ class Database(object):
             data = cur.fetchall()
             return data
 
+    @staticmethod
+    def queryParser(query):
+        if isinstance(query, type(None)):
+            query = ''
+        elif isinstance(query, str):
+            query = 'WHERE ' + query
+        return query
+
+    @staticmethod
+    def columnsParser(columns):
+        if isinstance(columns, type(None)):
+            columns = '*'
+        elif isinstance(columns, list):
+            columns = ', '.join(columns)
+        return columns
+
 
 if __name__ == "__main__":
     db = Database()
 
-    columns = {"id": "INTEGER PRIMARY KEY", "name": "TEXT", "price": "INT"}
+    columns = {"id": "TEXT", "name": "TEXT", "price": "INT"}
+    db.delete_table("cars")
+    db.create_table("cars", columns)
 
-    db.create_table("cars", columns, purge=True)
-    db.insert("cars", [(1, 'Audi', 52642),
-                       (2, 'BMW', 51642),
-                       (3, 'Hummer', 1642), ])
+    db.insert("cars", [(str(uuid.uuid4()), 'Audi', 52642),
+                       (str(uuid.uuid4()), 'BMW', 51642),
+                       (str(uuid.uuid4()), 'Hummer', 1642), ])
 
-    rows = db.fetchAll("cars", columns=['name', 'id'], query="price < 100")
+    db.remove("cars", query="name = 'Hummer'")
+    rows = db.fetch("cars", columns=['name', 'id'], query="price < 200000")
     print(rows)
